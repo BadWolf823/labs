3fd46c01b0bda0560ded41e99b553e3d5d3263e7
SeMurA823 <artyom.muraweow2018@yandex.ru>
2020-11-05 14:57:43 +0400

    add PublicClineable for one object

--- ru/muravyov/main/Main.java
+++ ru/muravyov/main/Main.java
@@ -14,9 +14,12 @@ public class Main{
     public static PublicCloneable[] clone(PublicCloneable ... obj){
         PublicCloneable[] objects = new PublicCloneable[obj.length];
         for(int i = 0; i < objects.length; i++){
-            objects[i] = (PublicCloneable) obj[i].clone();
+            objects[i] = clone(obj[i]);
         }
         return objects;
+    }
+    public static PublicCloneable clone(PublicCloneable obj){
+        return (PublicCloneable) obj.clone();
     }
 }
 
