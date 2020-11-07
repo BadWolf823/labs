7ce83f56e3d9f90cb934ba1faec9bc6d79c221b8
SeMurA823 <artyom.muraweow2018@yandex.ru>
2020-11-07 13:00:57 +0400

    example pattern BUILDER

--- ru/muravyov/main/Main.java
+++ ru/muravyov/main/Main.java
@@ -1,25 +1,73 @@
-
 package ru.muravyov.main;
-
-import ru.muravyov.numbers.Fraction;
 
 public class Main{
     public static void main(String[] args) {
-        Fraction fraction = new Fraction(3,1);
-        Fraction fraction1 = new Fraction(2,4);
-        Object[] objects = clone(fraction, fraction1);
-        for (Object obj : objects){
-            System.out.println(obj);
+        NameBuildable nameBuildable = Name.getBuilder()
+                .setFirstName("Иван")
+                .setPatronymic("Иванович");
+        Name name = nameBuildable.getName();
+        nameBuildable = Name.getBuilder();
+        Name name1 = nameBuildable.getName();
+        System.out.println(name);
+        System.out.println(name1);
+    }
+}
+
+class Name{
+    private String firstName, secondName, patronymic;
+
+    private static final NameBuilder nameBuilder = new NameBuilder();
+
+    private Name(String secondName, String firstName, String patronymic) {
+        this.firstName = firstName;
+        this.secondName = secondName;
+        this.patronymic = patronymic;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(
+                "%s %s %s",
+                (secondName!=null)?secondName:"",
+                (firstName != null)?firstName:"",
+                (patronymic != null)?patronymic:""
+                );
+    }
+
+    public static NameBuildable getBuilder(){
+        return nameBuilder;
+    }
+
+    private static class NameBuilder implements NameBuildable{
+        private String firstName, secondName, patronymic;
+        @Override
+        public NameBuilder setFirstName(String firstName) {
+            this.firstName = firstName;
+            return this;
+        }
+
+        @Override
+        public NameBuilder setSecondName(String secondName) {
+            this.secondName = secondName;
+            return this;
+        }
+
+        @Override
+        public NameBuilder setPatronymic(String patronymic) {
+            this.patronymic = patronymic;
+            return this;
+        }
+
+        @Override
+        public Name getName() {
+            return new Name(secondName, firstName, patronymic);
         }
     }
-    public static PublicCloneable[] clone(PublicCloneable ... obj){
-        PublicCloneable[] objects = new PublicCloneable[obj.length];
-        for(int i = 0; i < objects.length; i++){
-            objects[i] = clone(obj[i]);
-        }
-        return objects;
-    }
-    public static PublicCloneable clone(PublicCloneable obj){
-        return (PublicCloneable) obj.clone();
-    }
+}
+
+interface NameBuildable{
+    NameBuildable setFirstName(String firstName);
+    NameBuildable setSecondName(String secondName);
+    NameBuildable setPatronymic(String patronymic);
+    Name getName();
 }
